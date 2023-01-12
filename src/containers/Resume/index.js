309bc3cf1027792e345e429b6e118649827aaed8
adriank <adrian.cm.92@gmail.com>
2023-01-12 20:19:41 +0100

    Created a resume template

---
+++ src/containers/Resume/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+
+import Avatar from "../../components/Avatar";
+
+const Resume = () => {
+  return (
+    <div className="resume">
+      <Avatar />
+      <h1>Adrián Cañuelo Martínez</h1>
+    </div>
+  );
+};
+
+export default Resume;
